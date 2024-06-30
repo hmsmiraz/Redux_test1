1a237af12e9b63aa34720ec480295a641942b040
hmsmiraz <hmsmiraz64729@gmail.com>
2024-07-01 02:22:22 +0600

    added new

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { decrement, decrementByValue, increment } from "./redux/features/counterSlice";
+import {
+  decrement,
+  decrementByValue,
+  increment,
+  incrementByValue,
+} from "./redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 function App() {
@@ -7,6 +12,12 @@ function App() {
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <div className="flex border border-purple-300 bg-slate-100 p-10 rounded-md">
+        <button
+          onClick={() => dispatch(incrementByValue({ value: 5 }))}
+          className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white mr-3"
+        >
+          Increment 5
+        </button>
         <button
           onClick={() => dispatch(increment())}
           className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white"
--- src/redux/features/counterSlice.tsx
+++ src/redux/features/counterSlice.tsx
@@ -16,10 +16,14 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.count = state.count - 1;
     },
+    incrementByValue: (state, action) => {
+      state.count = state.count + action.payload.value;
+    },
     decrementByValue: (state, action: PayloadAction<number>) => {
       state.count = state.count - action.payload;
     },
   },
 });
-export const { increment, decrement, decrementByValue } = counterSlice.actions;
+export const { increment, decrement, incrementByValue, decrementByValue } =
+  counterSlice.actions;
 export default counterSlice.reducer;
